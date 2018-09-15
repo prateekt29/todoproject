76e4b44e0ff0c1b24353dffa02ebcfa25a6e1b09
prateekt29 <ptiwari2992@gmail.com>
2018-09-15 16:26:04 -0700

    Insert and list todo items done

--- postgres-express-react-node-tutorial/server/controllers/todos.js
+++ postgres-express-react-node-tutorial/server/controllers/todos.js
@@ -1,4 +1,5 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
   create(req, res) {
@@ -9,4 +10,16 @@ module.exports = {
       .then(todo => res.status(201).send(todo))
       .catch(error => res.status(400).send(error));
   },
+
+  list(req, res) {
+  return Todo
+    .findAll({
+      include: [{
+        model: TodoItem,
+        as: 'todoItems',
+      }],
+    })
+    .then(todos => res.status(200).send(todos))
+    .catch(error => res.status(400).send(error));
+},
 };
